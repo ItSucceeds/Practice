c01df6d93fc7e864563df2b1bd8fd9961de051fd
ItSucceeds <40326291+ItSucceeds@users.noreply.github.com>
2018-06-17 00:30:47 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,445 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<title>DirectTekenen.nl</title>
+	<meta charset="utf-8" />
+	<meta name="viewport" content="width=device-width, initial-scale=1" />
+	<link rel="stylesheet" type="text/css" href="css/vendor.bundle.css">
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+    <base href="https://directtekenen.nl/" />
+</head>
+<body class="theme-red theme-btn-round" data-spy="scroll" data-target="#navbar" data-offset="70">
+	<!-- Preloader -->
+	<!-- <div id="preloader">
+		<div id="status">&nbsp;</div>
+	</div> -->
+	
+	<!-- header -->
+	<header>
+		<!-- navbar -->
+		<nav id="navbar" class="navbar navbar-custom navbar-fixed-top" data-spy="affix" data-offset-top="100">
+			<div class="container">
+				<div class="row">
+					<!-- Brand and toggle get grouped for better mobile display -->
+					<div class="navbar-header page-scroll">
+						<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+						<i class="fa fa-bars"></i>
+						</button>
+						<a class="navbar-brand page-scroll logo-light" href="#"><img alt="" src="image/logo.png"></a>
+						<a class="navbar-brand page-scroll logo-clr" href="#"><img alt="" src="image/logo-clr.png"></a>
+					</div>
+
+					<!-- Collect the nav links, forms, and other content for toggling -->
+					<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+						<div class="right-nav text-right">
+							<ul class="nav navbar-nav menu">
+								<li>
+									<a href="#home">Home</a>
+								</li>
+								<li>
+								<li>
+									<a href="#feature">Mogelijkheden</a>
+								</li>
+								<li>
+									<a href="#overview">Handtekening</a>
+								</li>
+								<li>
+									<a href="#pricing">Tarieven</a>
+								</li>
+								<li>
+									<a href="#faq">FAQ</a>
+								</li>
+								<li>
+									<a href="https://platform.directtekenen.nl/index.php" target="_blank">Inloggen</a>
+								</li>
+								<li>
+									<a href="https://platform.directtekenen.nl/index.php?page=register" target="_blank">Aanmelden</a>
+								</li>
+							</ul>
+						</div>
+					</div>
+					<!-- /.navbar-collapse -->
+				</div>
+	        </div>
+		</nav>
+		<!-- End navbar -->
+
+		<!-- banner -->
+		<div id="home" class="hero parallaxie bg-gradient-4 hero-n-height overly" style='background: url("image/bg-parallax.jpg")'>
+			<div class="container h-100">
+				<div class="row hero-content flx-container h-100 text-center light">
+					<div class="intro-text w-100 align-flx-center">
+						<div class="spce lg"></div>
+						<div class="spce lg"></div>
+						<h1>DirectTekenen.nl</h1>
+						<p>Eenvoudig, Simpel &amp; Snel een overeenkomst sturen?<br />
+						Laat uw klant een digitale handtekening plaatsen voor een rechtsgeldig document.</p>
+						<div class="btn-holder">
+							<a class="btn btn-red" href="https://platform.directtekenen.nl/index.php?page=register" target="_blank">Direct Account Aanmaken</a>
+						</div>
+					</div>
+					<div class="img-pre align-flx-end text-center w-100">
+						<img alt="" src="image/img-g.png">
+					</div>
+				</div>
+			</div>
+		</div>
+	<!-- End Banner -->
+
+	</header>
+	<!-- End header -->
+
+
+	<!-- logo -->
+	<section class="sec-pad bg-grey">
+		<div class="container">
+			<div class="row">
+				<div class="logo-container row text-center">
+					<div class="col-md-3 ">
+						<img alt="" src="image/client-img-1.png">
+					</div>
+					<div class="col-md-3">
+						<img alt="" src="image/client-img-2.png">
+					</div>
+					<div class="col-md-3">
+						<img alt="" src="image/client-img-3.png">
+					</div>
+					<div class="col-md-3">
+						<img alt="" src="image/client-img-4.png">
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+	<!-- End logo-->
+
+	<!-- Feature -->
+	<section id="feature" class="sec-pad-lg">
+		<div class="container">
+			<div class="row">
+				<div class="section-text text-center">
+					<h4>Een digitale handtekening</h4>
+					<p>Dankzij onze dienst kunt u heel eenvoudig een document verzenden en rechtsgeldig laten ondertekenen.</p>
+				</div>
+				<div class="feature res-center">
+					<div class="col-md-4 col-sm-6 res-margin-sm animated wow fadeIn" data-wow-delay=".1s" data-wow-duration="2s">
+						<div class="icon icon-md">
+							<span class="ti-write"></span>
+						</div>
+						<h6>Makkelijk</h6>
+						<p>U kunt heel gemakkelijk een nieuw document aanmaken, versturen en later weer bekijken</p>
+					</div>
+					<div class="col-md-4 col-sm-6 res-margin-sm animated wow fadeIn" data-wow-delay=".2s" data-wow-duration="2s">
+						<div class="icon icon-md">
+							<span class="ti-package"></span>
+						</div>
+						<h6>Ontwerp sjablonen</h6>
+						<p>U kunt standaart sjablonen maken zodat u niet steeds het zelfde document hoeft te maken</p>
+					</div>
+					<div class="col-md-4 col-sm-6 res-margin-sm animated wow fadeIn" data-wow-delay=".3s" data-wow-duration="2s">
+						<div class="icon icon-md">
+							<span class="ti-harddrives"></span>
+						</div>
+						<h6>Simpel overzicht</h6>
+						<p>U heeft een simpel overzicht waarin u al uw documenten kunt beheren en bekijken</p>
+					</div>
+					<div class="spce"></div>
+					<div class="col-md-4 col-sm-6 animated wow fadeIn" data-wow-delay=".4s" data-wow-duration="2s">
+						<div class="icon icon-md">
+							<span class="ti-bookmark-alt"></span>
+						</div>
+						<h6>Digitale handtekening</h6>
+						<p>Door bijde partijen moeten de documenten worden ondertekend</p>
+					</div>
+					<div class="col-md-4 col-sm-6 res-margin-sm animated wow fadeIn" data-wow-delay=".1s" data-wow-duration="2s">
+						<div class="icon icon-md">
+							<span class="ti-mobile"></span>
+						</div>
+						<h6>Mobiel vriendelijk</h6>
+						<p>Ons platform werkt op de computer, telefoon &amp; tablet.</p>
+					</div>
+					<div class="col-md-4 col-sm-6 res-margin-sm animated wow fadeIn" data-wow-delay=".3s" data-wow-duration="2s">
+						<div class="icon icon-md">
+							<span class="ti-lock"></span>
+						</div>
+						<h6>Beveiligd</h6>
+						<p>Alle gegevens zijn beveiligd met een SSL encryptie</p>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+	<!-- End feature -->
+
+	<!-- Call action -->
+	<section class="subscribe sec-pad parallaxie bg-gradient-4 hero-n-height overly" style='background: url("image/bg-parallax.jpg")'>
+		<div class="container">
+			<div class="row">
+				<div class="section-text text-center light">
+					<h4>Kosten besparend</h4>
+					<p>Vergeet het over typen van overeenkomsten, het inscannen van documenten. Alles wordt digitaal bijgehouden</p>
+				</div>
+				<div class="col-md-8 col-md-offset-2 text-center">
+					<a class="btn btn-red" href="https://platform.directtekenen.nl/index.php?page=register" target="_blank">Account Aanmaken</a>
+				</div>
+			</div>
+		</div>
+	</section>
+	<!-- End Content -->
+
+	<!-- overview -->
+	<section id="overview" class="sec-pad">
+		<div class="container">
+			<div class="row">
+				<div class="overview flx-container align-flx-center">
+					<div class="col-sm-5">
+						<div class="animated wow fadeIn" data-wow-delay=".1s">
+							<h4>Een digitale handtekening met een digitaal vinger afdruk</h4>
+							<ul class="list-style">
+								<li>Juridisch Rechtsgeldig</li>
+								<li>Digitaal biometrische handtekening</li>
+								<li>Persoonlijk overzicht</li>
+								<li>Automatisch mail bevestiging</li>
+								<li>Opslaan als .PDF</li>
+							</ul>
+						</div>
+					</div>
+					<div class="col-sm-6 col-md-offset-1">
+						<div class="img">
+							<img alt="" src="image/img-dx.png">
+						</div>
+					</div>	
+				</div>
+				<div class="spce lg"></div>
+				<div class="spce lg"></div>
+				<div class="overview flx-container align-flx-center">
+					<div class="col-sm-6">
+						<div class="img">
+							<img alt="" src="image/img-d.png">
+						</div>
+					</div>
+					<div class="col-sm-5 col-md-offset-1">
+						<div class="animated wow fadeIn" data-wow-delay=".1s">
+							<h4>Ontwerp eigen sjablonen die u vaker kunt toepassen in uw document</h4>
+							<ul class="list-style">
+								<li>Sjablonen maken</li>
+								<li>Bekijk hoevaak de klant uw document inziet</li>
+								<li>Automatische herrinering na 3 dagen</li>
+							</ul>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+	<!-- End about app -->
+
+		<!-- Pricing -->
+	<section id="pricing" class="sec-pad bg-grey">
+		<div class="container">
+			<div class="row">
+				<div class="section-text text-center">
+					<div class="lead">Tarieven Abonnement</div>
+					<h4>Selecteer een pakket</h4>
+				</div>
+				<div class="pricing col-md-10 col-md-offset-1 text-center">
+					<div class="col-sm-4">
+						<div class="pricing-table res-margin-sm">
+							<div class="pricing-header">
+								<h6 class="fw-600">Pakket Klein</h6>
+							</div>
+							<div class="pricing-content">
+								<div class="main-pricing">
+									<h2 class="fw-600">&euro; 9,00</h2>
+									<p>Per Maand</p>
+								</div>
+								<div class="pricing-feature">
+									<ul>
+										<li>Juridisch Rechtsgeldig</li>
+										<li>50 Document per maand</li>
+										<li>2 Handtekeningen</li>
+										<li>PDF Download</li>
+										<li>Digitaal vinger afdruk</li>
+									</ul>
+								</div>
+							</div>
+							<div class="pricing-footer">
+								<a class="btn btn-red" href="https://platform.directtekenen.nl/index.php?page=register" target="_blank">Direct Starten</a>
+							</div>
+						</div>
+					</div>
+					<div class="col-sm-4">
+						<div class="pricing-table main res-margin-sm">
+							<div class="pricing-header">
+								<h6 class="fw-600">Pakket Middel</h6>
+							</div>
+							<div class="pricing-content">
+								<div class="main-pricing">
+									<h2 class="fw-600">&euro; 20,00</h2>
+									<p>Per Maand</p>
+								</div>
+								<div class="pricing-feature">
+									<ul>
+										<li>Juridisch Rechtsgeldig</li>
+										<li>200 Documenten</li>
+										<li>2 Handtekeningen</li>
+										<li>PDF Download</li>
+										<li>Digitaal vinger afdruk</li>
+									</ul>
+								</div>
+							</div>
+							<div class="pricing-footer">
+								<a class="btn btn-red" href="https://platform.directtekenen.nl/index.php?page=register" target="_blank">Direct Starten</a>
+							</div>
+						</div>
+					</div>
+					<div class="col-sm-4">
+						<div class="pricing-table">
+							<div class="pricing-header">
+								<h6 class="fw-600">Pakket Groot</h6>
+							</div>
+							<div class="pricing-content">
+								<div class="main-pricing">
+									<h2 class="fw-600">&euro; 45,00</h2>
+									<p>Per Maand</p>
+								</div>
+								<div class="pricing-feature">
+									<ul>
+										<li>Juridisch Rechtsgeldig</li>
+										<li>ONBEPERKT Document</li>
+										<li>2 Handtekeningen</li>
+										<li>PDF Download</li>
+										<li>Digitaal vinger afdruk</li>
+									</ul>
+								</div>
+							</div>
+							<div class="pricing-footer">
+								<a class="btn btn-red" href="https://platform.directtekenen.nl/index.php?page=register" target="_blank">Direct Starten</a>
+							</div>
+						</div>
+					</div>
+				</div>	
+                
+				<div class="section-text text-center">
+					<div class="lead">Geen abonnement nodig?</div>
+					<h4>Start met 1 document</h4>
+				</div>
+				
+				<div class="overview flx-container align-flx-center">
+					<div class="col-sm-5">
+						<div class="pricing col-md-12 col-md-offset-1 text-center" style="margin-left: 170px;">
+						<div class="pricing-table main res-margin-sm">
+							<div class="pricing-header">
+								<h6 class="fw-600">1 Document</h6>
+							</div>
+							<div class="pricing-content" style="padding-bottom: 30px;">
+								<div class="pricing-feature" style="margin-top: 40px;">
+									<ul>
+										<li>Juridisch Rechtsgeldig</li>
+										<li>1 Document</li>
+										<li>2 Handtekeningen</li>
+										<li>PDF Download</li>
+										<li>Digitaal vinger afdruk</li>
+									</ul>
+								</div>
+							</div>
+						</div>
+				</div>
+					</div>
+					<div class="col-sm-6 col-md-offset-1" style="text-align: center;">
+						
+								<div class="main-pricing">
+									<h2 class="fw-600">&euro; 4,00</h2>
+									<p>Eenmalig</p>
+								</div>
+								
+								<div class="pricing-footer">
+									<a class="btn btn-red" href="https://platform.directtekenen.nl/index.php?page=register" target="_blank">Direct Starten</a>
+								</div>
+					</div>	
+				</div>
+			</div>
+		</div>
+	</section>
+	<!-- End Pricing -->
+	<!-- Counter -->
+	<section class=" sec-pad-lg">
+		<div class="container">
+			<div class="row">
+				<div class="section-text text-center">
+					<div class="lead">Statistieken</div>
+					<h4>Bekijk de aantalen in ons systeem.</h4>
+				</div>
+				<div class="static-warapper col-md-8 col-md-offset-2 clearfix text-center">
+					<div class="col-sm-4 counter-wrapper res-margin-sm">
+						<div class="top-row">
+							<h2 class="counter clr-red">31365</h2>
+						</div>
+						<p>Documenten</p>
+					</div>
+					<div class="col-sm-4 counter-wrapper res-margin-sm">
+						<div class="top-row">
+							<h2 class="counter clr-red">1652</h2>
+						</div>
+						<p>Gebruikers</p>
+					</div>
+					<div class="col-sm-4 counter-wrapper">
+						<div class="top-row">
+							<h2 class="counter clr-red">24659</h2>
+						</div>
+						<p>Documenten ondertekend</p>
+					</div>					
+				</div>
+			</div>
+		</div>
+	</section>
+	<!-- End counter -->
+
+
+	<!-- Faq section -->
+	<section id="faq" class="sec-pad">
+		<div class="container demo">
+			<div class="row">
+				<div class="section-text text-center">
+					<div class="lead">Faqs</div>
+					<h4>Veel gestelde vragen</h4>
+				</div>
+				<div class="faq">
+					<div class="col-md-6 res-margin-xs">
+						<h6>Werkt DirectTekenen.nl op elk apparaat?</h6>
+						<p>Dit platform werkt perfect op de computer, smartphone &amp; tablet</p>
+					</div>
+					<div class="col-md-6">
+						<h6>Kan een ander een vertrouwelijk document inzien?</h6>
+						<p>Alleen u en de gemachtigde kan het document inzien door middel van een beveiligd wachtwoord</p>
+					</div>
+					<div class="spce"></div>
+					<div class="col-md-12 res-margin-xs">
+						<h6>Hoe bespaar ik hiermee kosten?</h6>
+						<p>Doordat u gebruik maakt van digitale formulieren wordt alles voor u opgeslagen. U hoeft weinig nog te printen. Opnieuw over te typen (back-office) waardoor er ook geen fouten meer worden gemaakt.</p>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+	<!-- End faq -->
+	
+	<!--Footer  -->
+	<footer class="footer-wrapper">
+		<div class="container">
+			<div class="row">
+					<div class="copyright col-md-6 text-center res-center">
+						&copy; 2017 COPYRIGHT <strong>DIRECTTEKENEN.NL</strong> . ALLE RECHTEN VOORBEHOUDEN
+					</div>			
+				</div>
+			</div>			
+		</div>
+	</footer>
+	<!-- End Footer -->
+
+	<!-- script -->
+	<script src="js/vendor.bundle.js"></script>
+	<script src="js/script.js"></script>
+
+</body>
+</html>
